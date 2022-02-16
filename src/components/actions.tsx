2952d6108d79f05c1b75b9a9cfeb1199e3be5af4
Kelly <guojiyou40020@hotmail.com>
2022-02-16 20:57:46 +0800

    added actions section

---
+++ src/components/actions.tsx
@@ -0,0 +1,9 @@
+import {List, Icon} from "@raycast/api";
+
+export default function Actions() {
+  return (
+    <List.Section>
+      <List.Item title="Goto Repo" icon={{source: "book.svg"}} />
+    </List.Section>
+  )
+}
--- src/kl.tsx
+++ src/kl.tsx
@@ -1,10 +1,12 @@
 import { List } from "@raycast/api";
+import Actions from "./components/actions";
 import Summary from "./components/summary";
 
 export default function KellyWorkflow() {
   return (
     <List>
       <Summary />
+      <Actions />
     </List>
   );
 }
